7d9de8e977861a38f50b817c60de34d8500b17fc
Anggi Eko Pambudi <81364763+gemgum@users.noreply.github.com>
2021-04-04 08:14:35 +0700

    Update javanan.js

--- js/javanan.js
+++ js/javanan.js
@@ -1,16 +1,16 @@
-// $('.skrol').on('click', function(e){
+$('.skrol').on('click', function(e){
      
-//     //ambil isi href
-//     var refs = $(this).attr('href');
-//     //cekel elemen href
-//     // console.log(refs);
-//     var ele = $(refs);
-//     //console.log(ele.offset().top);     
+    //ambil isi href
+    var refs = $(this).attr('href');
+    //cekel elemen href
+    // console.log(refs);
+    var ele = $(refs);
+    //console.log(ele.offset().top);     
 
-//     $('html,body').animate({
-//         scrollTop: ele.offset().top - 50
+    $('html,body').animate({
+        scrollTop: ele.offset().top - 50
 
-//     },1000,'swing');
+    },1000,'swing');
 
-//     e.preventDefault();
-// });
+    e.preventDefault();
+});
